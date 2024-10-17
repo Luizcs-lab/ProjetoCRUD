4ceb5e339773066f1c0f38998229876018f29e16
Auto Logon <autologon@CSED.LAB>
2024-10-17 19:46:46 -0300

    fazendo alteracoes

---
+++ ProjetoCRUD-main/create.php
@@ -0,0 +1,10 @@
+<?php
+if(isset($_POST["nome"]) && isset($_POST["email"]) && isset($_POST["idade"] )){
+
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $idade = $_POST['idade'];
+
+}
+
+?>
