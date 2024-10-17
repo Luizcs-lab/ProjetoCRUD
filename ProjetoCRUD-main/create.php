f568c921b7cd7b621ad837867490cb1384f8c655
Auto Logon <autologon@CSED.LAB>
2024-10-17 20:01:59 -0300

    fazendo a funcao de create em php

--- ProjetoCRUD-main/create.php
+++ ProjetoCRUD-main/create.php
@@ -1,10 +1,11 @@
 <?php
 if(isset($_POST["nome"]) && isset($_POST["email"]) && isset($_POST["idade"] )){
 
-    $nome = $_POST['nome'];
-    $email = $_POST['email'];
-    $idade = $_POST['idade'];
+    $nome[] = $_POST['nome'];
+    $email[] = $_POST['email'];
+    $idade[] = $_POST['idade'];
 
+    echo "Ola".$_POST["nome"].""$_POST["email"].$_POST["idade"];
 }
 
 ?>
